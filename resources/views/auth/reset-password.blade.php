9be7e82617ba1db5936db6e3aef1214926770a82
Adenekan Martin <37469720+doubleprincez@users.noreply.github.com>
2021-07-05 12:46:52 +0100

    Update Reset Password Page

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -13,7 +13,6 @@
                             @csrf
 
                             <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
                             <div class="form-group mb-3 block">
                                 <x-jet-label for="email" value="{{ __('Email') }}"/>
                                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email"
