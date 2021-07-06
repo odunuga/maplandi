b79ce353b4b1574286addf283db848ff4a1edc07
Adenekan Martin <37469720+doubleprincez@users.noreply.github.com>
2021-07-06 19:19:27 +0100

    Creating Cart Modal, Counter and Cart Dynamic Page

---
+++ resources/views/livewire/cart-modal.blade.php
@@ -0,0 +1,52 @@
+<!-- CART-->
+<div class="modal modal-dialog-scrollable fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title" id="exampleModalLabel">
+                    <i class="lni lni-cart-full" style="color:red; font-size:25px;"></i>
+                    Your Cart</h6>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <ol class="list-group list-group-numbered">
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="ms-2 me-auto">
+                            <div class="text-dark"><b>Product Name </b></div>
+                            Category : Phones
+                        </div>
+                        <div class="text-dark"><b>$200 </b></div>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="ms-2 me-auto">
+                            <div class="text-dark"><b>Product Name </b></div>
+                            Category : Phones
+                        </div>
+                        <div class="text-dark"><b>$200 </b></div>
+                    </li>
+
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="ms-2 me-auto">
+                            <div class="text-dark"><b>Product Name </b></div>
+                            Category : Phones
+                        </div>
+                        <div class="text-dark"><b>$200 </b></div>
+                    </li>
+
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="ms-2 me-auto">
+                            <div class="text-dark"><b>TOTAL</b></div>
+                        </div>
+                        <div class="text-dark"><b>$600 </b></div>
+                    </li>
+
+                </ol>
+            </div>
+            <div class="modal-footer">
+                <a class="btn" href="cart" class="btn btn-dark btn-lg ">
+                    View Cart</a>
+            </div>
+        </div>
+    </div>
+</div>
