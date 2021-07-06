6e7819f2118eac2843d36e2c571eabb39e527c35
Adenekan Martin <37469720+doubleprincez@users.noreply.github.com>
2021-07-06 13:43:00 +0100

    Making Why Choose Us dynamic i.e can be changed from admin backend and adding testimonial page with demo users

--- app/View/Components/FeaturesComponent.php
+++ app/View/Components/FeaturesComponent.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class Features extends Component
+class FeaturesComponent extends Component
 {
     /**
      * Create a new component instance.
--- app/View/Components/TestimonialComponent.php
+++ app/View/Components/TestimonialComponent.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class testimonialComponent extends Component
+class TestimonialComponent extends Component
 {
     /**
      * Create a new component instance.
@@ -23,6 +23,6 @@ class testimonialComponent extends Component
      */
     public function render()
     {
-        return view('components.testimonialComponent');
+        return view('components.testimonial');
     }
 }
