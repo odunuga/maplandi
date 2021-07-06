2f9e032ba4aea60f6e1981dec7c773589e69972f
Adenekan Martin <37469720+doubleprincez@users.noreply.github.com>
2021-07-06 19:26:22 +0100

    Updating Features Component

--- resources/views/components/features.blade.php
+++ resources/views/components/features.blade.php
@@ -1,3 +1,43 @@
-<div>
-    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
-</div>
+    <section class="how-works section bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title">
+                        <h2 class="wow fadeInUp" data-wow-delay=".4s">Features </h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-12">
+
+                    <div class="single-work wow fadeInUp" data-wow-delay=".2s">
+<span class="serial" style="border: 6px solid #dc3545;">
+<i class="lni lni-support"></i>
+</span>
+                        <h3>24/7 Customer Support</h3>
+                    </div>
+
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+
+                    <div class="single-work wow fadeInUp" data-wow-delay=".4s">
+<span class="serial" style="border: 6px solid #dc3545;">
+<i class="lni lni-checkmark-circle"></i>
+</span>
+                        <h3>Best Quality</h3>
+                    </div>
+
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+
+                    <div class="single-work wow fadeInUp" data-wow-delay=".6s">
+<span class="serial" style="	border: 6px solid #dc3545;">
+&#x20A6;
+</span>
+                        <h3>Affordable Prices</h3>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
